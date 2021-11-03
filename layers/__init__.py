b747fa4286a703ce5d5c282474a5f7ff2e851dbb
RuixZh <38558445+RuixZh@users.noreply.github.com>
2021-11-03 13:59:46 +0100

    Delete layers directory

--- layers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .GCN import GCN, GNN
-from .discriminator import Discriminator, BiLinear
-from .attention import Attention, NodeAttention, SemanticAttention, LocalAttention
-from .fc import FullyConnect
-from .GCN2 import DGCN
